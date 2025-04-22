e316cd1fa1fb2ce6029dfdae37a8fdef0b8ac61c
scr1mer-41 <nik.malov1990@gmail.com>
2025-04-22 16:10:02 +0300

    Update CdCommand.java

--- ShellWithAnnotations/src/main/java/commands/CdCommand.java
+++ ShellWithAnnotations/src/main/java/commands/CdCommand.java
@@ -12,29 +12,24 @@ public class CdCommand implements Command {
 
     @Override
     public void execute(String[] args) throws CommandExecutionException {
-        // Если аргументов нет, переходим в домашнюю директорию
         if (args.length == 0) {
             String homeDir = System.getProperty("user.home");
             changeDirectory(homeDir);
             return;
         }
 
-        // Получаем путь из первого аргумента
         String path = args[0];
         changeDirectory(path);
     }
 
     private void changeDirectory(String path) throws CommandExecutionException {
         try {
-            // Обрабатываем специальный символ ~ (домашняя директория)
             if (path.startsWith("~")) {
                 path = path.replaceFirst("~", System.getProperty("user.home"));
             }
 
-            // Получаем абсолютный путь
             Path newPath = Paths.get(path).toAbsolutePath().normalize();
 
-            // Проверяем, что путь существует и это директория
             if (!Files.exists(newPath)) {
                 throw new CommandExecutionException("cd", "Директория не существует: " + newPath);
             }
@@ -43,7 +38,6 @@ public class CdCommand implements Command {
                 throw new CommandExecutionException("cd", "Указанный путь не является директорией: " + newPath);
             }
 
-            // Меняем рабочую директорию
             System.setProperty("user.dir", newPath.toString());
 
         } catch (SecurityException e) {
